755da18cc7409366aeb8b3607b96c52db56bb144
Reece Benson <business@reecebenson.me>
2019-07-29 13:55:53 +0000

    Fixes & deployment with default files

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 # Automatically deploy a dev, staging and live environment structure setup for NGINX
 # Follows the site structure of /var/sites/{live,staging,development}/{sitename}/{public,logs}
 # Follows the NGINX structure of /etc/sites/{live,staging,development}/{live,stg,dev}.{sitename}
+SITE_ROOT=/var/sites
 
 echo "Please enter the site name:"
 read SITE_NAME
@@ -9,7 +10,7 @@ read SITE_NAME
 echo "Please enter the site URL:"
 read SITE_URL
 
-cd /var/sites
+cd $SITE_ROOT
 
 # Make the development, staging and live structure
 mkdir development/$SITE_NAME development/$SITE_NAME/logs development/$SITE_NAME/public
@@ -17,8 +18,29 @@ mkdir staging/$SITE_NAME staging/$SITE_NAME/logs staging/$SITE_NAME/public
 mkdir live/$SITE_NAME live/$SITE_NAME/logs live/$SITE_NAME/public
 echo "Created $SITE_NAME development, staging and live structures."
 
+# Create the site default files
+cd $SITE_ROOT/.deploy/
+if [ -d "default_files" ]; then
+  cp ./default_files/* ../development/$SITE_NAME/public
+  cp ./default_files/* ../staging/$SITE_NAME/public
+  cp ./default_files/* ../live/$SITE_NAME/public
+
+  # Edit the default files -- there is probably a nicer way to do this...
+  cd $SITE_ROOT/development/$SITE_NAME/public
+  find . -name "*.php" -exec sed -i -e "s/SITENAME/$SITE_NAME/g" {} \;
+  find . -name "*.php" -exec sed -i -e "s/SITEURL/$SITE_URL/g" {} \;
+
+  cd $SITE_ROOT/staging/$SITE_NAME/public
+  find . -name "*.php" -exec sed -i -e "s/SITENAME/$SITE_NAME/g" {} \;
+  find . -name "*.php" -exec sed -i -e "s/SITEURL/$SITE_URL/g" {} \;
+
+  cd $SITE_ROOT/live/$SITE_NAME/public
+  find . -name "*.php" -exec sed -i -e "s/SITENAME/$SITE_NAME/g" {} \;
+  find . -name "*.php" -exec sed -i -e "s/SITEURL/$SITE_URL/g" {} \;
+fi
+
 # Create the NGINX Site
-cd ./.deploy/
+cd $SITE_ROOT/.deploy/
 mkdir $SITE_NAME
 cp nginxsite $SITE_NAME/dev.$SITE_NAME
 cp nginxsite $SITE_NAME/stg.$SITE_NAME
--- remove.sh
+++ remove.sh
@@ -5,7 +5,7 @@
 echo "Enter the site name to remove:"
 read SITE_NAME
 
-rm /etc/sites/*/*.$SITE_NAME && rm -d /var/sites/.deploy/$SITE_NAME/ && rm -rf /var/sites/*/$SITE_NAME
+rm /etc/sites/*/*.$SITE_NAME && rm -rf /var/sites/*/$SITE_NAME
 echo "Cleaned up directories and nginx configuration for: $SITE_NAME"
 
 echo "Restarting nginx (requires privileges)..."
